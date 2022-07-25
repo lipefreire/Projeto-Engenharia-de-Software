f0da28d4781979f3e3147cbc811ae456f85dd0d9
Felipe Freire <86164099+lipefreire@users.noreply.github.com>
2022-07-25 14:29:06 -0300

    aula 25-07

--- EstudoDeCaso/src/main/java/com/mycompany/estudodecaso/Controle.java
+++ EstudoDeCaso/src/main/java/com/mycompany/estudodecaso/Controle.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.estudodecaso;
 
-/**
- *
- * @author Aluno
- */
 public class Controle {
-    
+    public static void main(String[] args) throws Exception {
+        EstudoDAO estudoDAO = new EstudoDAO();
+        Estudo es = new Estudo();
+        
+        es.setDataEmissaoProtocolo("2022-07-25");
+        es.setDataInicioExperimento("2022-07-25");
+        es.setDataFimExperimento("2022-07-25");
+        es.setJustificativaUsoAnimais("ASDASD");
+        es.setResumoPortugues("portugues");
+        es.setResumoIngles("ingles");
+        
+        estudoDAO.emitir(es);
+    }
 }
--- EstudoDeCaso/src/main/java/com/mycompany/estudodecaso/EstudoDAO.java
+++ EstudoDeCaso/src/main/java/com/mycompany/estudodecaso/EstudoDAO.java
@@ -7,11 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EstudoDAO {
-    Public Conexao conexao;
+    public Conexao conexao;
     
     private Connection con;
     private PreparedStatement psts;
     private ResultSet rs;
     
+    public EstudoDAO() {
+        conexao = new Conexao();
+    }
     
+    public void emitir(Estudo e) throws Exception {
+        String sql = "INSERT INTO protocolo (dataEmissaoProtocolo, dataInicioExperimento, dataFimExperimento, JustificativaUsoAnimais, resumoPortugues, resumoIngles) VALUES (?, ?, ?, ?, ?, ?)";
+        
+        con = Conexao.conexaoMySQL();
+        psts = con.prepareStatement(sql);
+        
+        psts.setString(1, e.getDataEmissaoProtocolo());
+        psts.setString(2, e.getDataInicioExperimento());
+        psts.setString(3, e.getDataFimExperimento());
+        psts.setString(4, e.getJustificativaUsoAnimais());
+        psts.setString(5, e.getResumoPortugues());
+        psts.setString(6, e.getResumoIngles());
+        psts.execute();
+    }
 }
